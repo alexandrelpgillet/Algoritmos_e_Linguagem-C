4fc2dce566818179648e8277c1379a79c162a46b
Alexandre Laureano Prata Gillet <57054750+alexandrelpgillet@users.noreply.github.com>
2024-05-22 17:49:56 -0300

    add contend in abertura_de_arquivos.c

---
+++ Linguagem C/Manipulação de arquivos/Arquivos TXT/abertura_de_arquivos.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(){
+
+
+
+    FILE *arquivo; // variavel do tipo FILE sempre será ponteiro;
+
+    arquivo = fopen("nome_arquivo.txt" , "w");
+
+    /*
+    
+     1)Declarar um ponteiro do tipo FILE e realizar a abertura do arquivo .txt , atráves do comando :
+     
+        - fopen("nome_arquivo.txt", "modo de abertura do arquivo ");
+
+     2)Modos de abertura do arquivo :
+
+        -"w" = modo de escrita(write) , caso não exista o arquivo com o determinado nome ,  o arquivo será criado , caso já exista o arquivo já existente sera apagado e criado novamente;
+
+        
+        -"r" = modo de leitura (read) , caso não exista o arquivo com o determinado nome , a variavel arquivo retornará NULL , caso exista o arquivo será aberto para a leitura;
+
+        
+        -"a"= modo de leitura e escrita no final do arquivo (ANEXO / APPEND ), caso não exista o arquivo com o determinado nome a variavel arquivo retornará null;
+
+
+        -"w+" = modo de escrita e leitura , caso não exista o arquivo com o determinado nome ,  o arquivo será criado , caso já exista o arquivo já existente sera apagado e criado novamente;
+
+        
+        -"r+" = modo de leitura e escrita ,  caso não exista o arquivo com o determinado nome ,  a variavel arquivo retornará NULL , caso já exista será aberto tanto para a leitura quanto para escrita.
+
+        
+        -"a+" = modo de leitura e escritaa no final do arquivo (ANEXO/APPEN) , caso não exista o arquivo com o determinado nome , o arquivo será criado , caso já exista o arquivo , o ponteiro de leitura/escrita do arquivo será deslocado para o final do arquivo;
+
+
+
+
+
+    
+    */
+
+   fclose(arquivo); // Função para fechar o arquivo 
+
+
+
+
+
+
+
+}
